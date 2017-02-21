0a91869845fbc4df96b8ca16fc99a770eaab419f
Anders Pearson <anders@columbia.edu>
2017-02-21 19:12:09 +0000

    triangle_area()

--- week1/second.erl
+++ week1/second.erl
@@ -1,8 +1,11 @@
 -module(second).
--export([hypotenuse/2, triangle_perimeter/2]).
+-export([hypotenuse/2, triangle_perimeter/2, triangle_area/2]).
 
 hypotenuse(A,B) ->
     math:sqrt(first:square(A) + first:square(B)).
 
 triangle_perimeter(A,B) ->
     A + B + hypotenuse(A,B).
+
+triangle_area(A,B) ->
+    (A * B) / 2.
