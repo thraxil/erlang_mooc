1f76835ce6270366ce6ffac53bebb4b767bf557e
Anders Pearson <anders@columbia.edu>
2017-02-26 20:05:08 +0000

    tail recursive fib w/ accumulators

--- week1/fibs.erl
+++ week1/fibs.erl
@@ -1,5 +1,5 @@
 -module(fibs).
--export([fib/1]).
+-export([fib/1, fibTD/1]).
 
 fib(0) -> 0;
 fib(1) -> 1;
@@ -23,3 +23,21 @@ fib(N) -> fib(N - 1) + fib(N - 2).
 %  -> (2)  + (1          + 0).
 %  -> (2)  + (1).
 %  -> 3.
+
+fibT(0, _, Acc2) -> Acc2;
+fibT(1, Acc1, _) -> Acc1;
+fibT(N, Acc1, Acc2) ->
+    fibT(N - 1, Acc1 + Acc2, Acc1).
+
+fibTD(N) ->
+    fibT(N, 1, 0).
+
+% fibTD(4)
+% -> fibT(4, 1, 0)
+% -> fibT(4 - 1, 1 + 0, 1)
+% -> fibT(3, 1, 1)
+% -> fibT(3 - 1, 1 + 1, 1)
+% -> fibT(2, 2, 1)
+% -> fibT(2 - 1, 2 + 1, 2)
+% -> fibT(1, 3, 2)
+% -> 3
