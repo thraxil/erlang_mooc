9972b275cb2b5bfe946ee5ee606fa0c2e6c53eb9
Anders Pearson <anders@columbia.edu>
2017-02-28 22:47:19 +0000

    evens/1

--- week2/ex2.erl
+++ week2/ex2.erl
@@ -1,7 +1,15 @@
 -module(ex2).
--export([double/1]).
+-export([double/1, evens/1]).
 
 double([]) ->
     [];
 double([X|Xs]) ->
     [X * 2|double(Xs)].
+
+
+evens([]) ->
+    [];
+evens([X|Xs]) when X rem 2 == 0 ->
+    [X|evens(Xs)];
+evens([_X|Xs]) ->
+    evens(Xs).
